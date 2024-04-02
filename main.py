fdf8588f596fed48a3701ad3380fb1144bc43562
Mrs James <lollypopstar70@gmail.com>
2024-04-02 09:48:54 +0000

    Turned this into  a QR code maker

---
+++ main.py
@@ -0,0 +1,11 @@
+#https://www.codedex.io/projects/generate-a-qr-code-with-python
+import qrcode
+
+website_link = 'https://www.geekcandy.co.uk'
+
+qr = qrcode.QRCode(version = 5, box_size = 5, border = 0)
+qr.add_data(website_link)
+qr.make()
+
+img = qr.make_image(fill_color="orange", back_color="white")#colours dont seem to work
+img.save('geekcandy_QR.png')
